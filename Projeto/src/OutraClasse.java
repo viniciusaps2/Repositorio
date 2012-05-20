afb37ac4cc43b8854be4b6d419361dee3018a865
Vinicius Souza <vinicius.apsouza@gmail.com>
2012-05-20 11:43:13 -0300

    quarto commit

--- Projeto/src/OutraClasse.java
+++ Projeto/src/OutraClasse.java
@@ -1,12 +1,12 @@
 
-public class Teste {
+public class OutraClasse {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("lollolol");
+		System.out.println("lol");
 	}
 
 }
